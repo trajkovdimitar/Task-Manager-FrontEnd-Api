1a8997b413b5370eb50063fcf0757babf8e2a718
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2019-10-26 00:07:07 +0200

    Add files via upload

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+import { NewListComponent } from './components/new-list/new-list.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { EditListComponent } from './components/edit-list/edit-list.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/lists', pathMatch: 'full' },
+  { path: 'new-list', component: NewListComponent },
+  { path: 'edit-list/:listId', component: EditListComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'lists', component: TaskViewComponent },
+  { path: 'lists/:listId', component: TaskViewComponent },
+  { path: 'lists/:listId/new-task', component: NewTaskComponent },
+  { path: 'lists/:listId/edit-task/:taskId', component: EditTaskComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+import { NewListComponent } from './components/new-list/new-list.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { WebReqInterceptor } from './web-req.interceptor';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { EditListComponent } from './components/edit-list/edit-list.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+    NewListComponent,
+    NewTaskComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    EditListComponent,
+    EditTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: WebReqInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
